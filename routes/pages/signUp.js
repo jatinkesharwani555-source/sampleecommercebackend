30a077fd432df0fabd5e63b411c7bafbc0d10d24
Jatin Kesharwani <krishnakesharwani555@gmail.com>
2026-03-11 15:54:06 +0530

    changessss

--- routes/pages/signUp.js
+++ routes/pages/signUp.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const routerController = require("../../controller/routerController");
+const multerOptions = require("../../config/multer");
 const signUpRouter = express.Router();
 
-signUpRouter.post("/api/signup", routerController.signUpRouterController);
+signUpRouter.post("/api/signup", multerOptions.single("userImage"), routerController.signUpRouterController);
 
 module.exports = signUpRouter;
